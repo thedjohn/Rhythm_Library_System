99bc13fb7f0b95ecd4b84e4219eee6c1326ed241
thedjohn <thedjohn@users.noreply.github.com>
2026-06-25 17:37:36 +0000

    deploy: e875e3ff295802a19484b5f6ac44ff5a3f9b4485

--- js/notation.js
+++ js/notation.js
@@ -156,7 +156,7 @@ window.rhythmNotation = (() => {
             ctx.moveTo(x+rx-1, staffY-ry+1); ctx.lineTo(x-rx+1, staffY+ry-1);
             ctx.stroke();
             // Accent mark (>) above the note
-            const aY = -20;
+            const aY = -5;
             ctx.lineWidth = 1.3; ctx.lineJoin = 'miter';
             ctx.beginPath();
             ctx.moveTo(x - 5, aY + 4); ctx.lineTo(x, aY - 4); ctx.lineTo(x + 5, aY + 4);
